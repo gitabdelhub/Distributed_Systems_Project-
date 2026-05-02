8150041650958071ecaeab6a9061d761da57c457
Abdallah ASSOUMANOU <abdalahassoumanoul@gmail.com>
2026-05-02 17:12:07 +0100

    Fix component scan and update README

--- server/src/main/java/com/monitor/server/core/ServerMain.java
+++ server/src/main/java/com/monitor/server/core/ServerMain.java
@@ -3,7 +3,7 @@ package com.monitor.server.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.monitor.server")
 public class ServerMain {
     public static void main(String[] args) {
         SpringApplication.run(ServerMain.class, args);
