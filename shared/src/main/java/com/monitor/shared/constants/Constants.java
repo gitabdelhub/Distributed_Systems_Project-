9bcb76f00fee2174cae601710eaba9718e0bf4f3
ASSOUMANOU Abdallah <abdallahassoumanou@gmail.com>
2026-05-02 17:17:24 +0100

    Refactor constants to use Network and Threshold constants

--- shared/src/main/java/com/monitor/shared/constants/Constants.java
+++ shared/src/main/java/com/monitor/shared/constants/Constants.java
@@ -1,12 +1,12 @@
 package com.monitor.shared.constants;
 
 public class Constants {
-    public static final int UDP_PORT = 9000;
-    public static final int TCP_PORT = 9001;
-    public static final int RMI_PORT = 1099;
-    public static final String RMI_SERVICE_NAME = "MetricsService";
-    public static final int AGENT_SEND_INTERVAL_MS = 5000;
-    public static final double CPU_ALERT_THRESHOLD = 90.0;
-    public static final double RAM_ALERT_THRESHOLD = 85.0;
-    public static final double DISK_ALERT_THRESHOLD = 95.0;
+    public static final int UDP_PORT = NetworkConstants.PORT_UDP;
+    public static final int TCP_PORT = NetworkConstants.PORT_TCP;
+    public static final int RMI_PORT = NetworkConstants.PORT_RMI;
+    public static final String RMI_SERVICE_NAME = NetworkConstants.RMI_SERVICE_NAME;
+    public static final int AGENT_SEND_INTERVAL_MS = ThresholdConstants.AGENT_SEND_INTERVAL_MS;
+    public static final double CPU_ALERT_THRESHOLD = ThresholdConstants.CPU_CRITICAL;
+    public static final double RAM_ALERT_THRESHOLD = ThresholdConstants.RAM_CRITICAL;
+    public static final double DISK_ALERT_THRESHOLD = ThresholdConstants.DISK_CRITICAL;
 }
